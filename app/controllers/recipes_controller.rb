53433e495cfa082808873342099807a6570d360d
QueenTesa Fuggett <qfuggett@umich.edu>
2020-11-14 15:17:40 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,24 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(ingredient_type: '1')
+    @recipe.ingredients.build(ingredient_type: '2')
   end
 
   def create
+    Recipe.create(recipe.params)
+    redirect_to recipe_path
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :title,
+        :quantity
+      ]
+    )
+    end
 end
